3f520cb23e1c6558b8d004d3e717e0ff270be449
andyputra12 <andywolf3653@gmail.com>
2017-02-01 12:47:18 +0700

    JavaClass

---
+++ simplecalculator/app/src/main/java/com/example/d2j_00/simplecalculator/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.d2j_00.simplecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void calculator (View view)
+    {
+        EditText num1=(EditText) findViewById(R.id.num1)
+        EditText num2=(EditText) findViewById(R.id.num2)
+        TextView result=(Textview)
+    }
+}
